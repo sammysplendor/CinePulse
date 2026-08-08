53d4916fd6ad079065546bb2d217cee668c7f35f
sammysplendor <splendidsam006@gmail.com>
2026-08-08 23:11:16 +0100

    Make MovieCard title and image a Link

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,25 @@
 import styles from "./MovieCard.module.css";
 import { IMAGE_BASE_URL, IMAGE_SIZES } from "../constants/config";
 import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ movie, onAddToWatchlist }) => {
   if (!movie?.poster_path) return null;
 
   return (
     <div className={styles.cardContainer}>
-      <img
-        src={`${IMAGE_BASE_URL}${IMAGE_SIZES.poster_sm}${movie.poster_path}`}
-        alt={movie.title}
-        className={styles.poster}
-      />
-      <p className={styles.title}>
-        <b>{movie.title}</b>
-      </p>
+      <Link to={`/movie/${movie.id}`} title="See full detail">
+        <img
+          src={`${IMAGE_BASE_URL}${IMAGE_SIZES.poster_sm}${movie.poster_path}`}
+          alt={movie.title}
+          className={styles.poster}
+        />
+      </Link>
+      <Link to={`/movie/${movie.id}`} title="See full detail">
+        <p className={styles.title}>
+          <b>{movie.title}</b>
+        </p>
+      </Link>
       <p>{new Date(movie.release_date).getFullYear()}</p>
 
       <button onClick={() => onAddToWatchlist(movie)}>

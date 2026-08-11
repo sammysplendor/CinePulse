7a727b5140b625e9aab462bb49456fba522d1a39
sammysplendor <splendidsam006@gmail.com>
2026-08-11 18:49:54 +0100

    Change image backdrop value

--- src/constants/config.js
+++ src/constants/config.js
@@ -5,5 +5,5 @@ export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 export const IMAGE_SIZES = {
   poster_sm: "w342",
   poster_lg: "w500", // for cards
-  backdrop: "original", // for hero
+  backdrop: "w1280", // for hero
 };
